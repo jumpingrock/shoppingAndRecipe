2e1c8188446f3499d340e496ef26b8ab966e7b5c
jumpingrock <kennethyeongky@gmail.com>
2019-05-06 23:29:11 +0800

    added header

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  receipes: Recipe[] = [new Recipe('A Test Recipe', 'This is a test', 'https://i1.wp.com/ohmyveggies.com/wp-content/uploads/2013/02/how_to_make_mushroom_stock.jpg')];
+
 
   constructor() { }
 
