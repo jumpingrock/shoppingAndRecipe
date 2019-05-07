561cbdd4c346a64938b7565fa2d2d6babbb00bf1
jumpingrock <kennethyeongky@gmail.com>
2019-05-08 00:41:59 +0800

    ngFor and databinding

---
+++ src/app/shared/ingredient.mode.ts
@@ -0,0 +1,11 @@
+export class Ingredient {
+  // public name: string;
+  // public amount: string;
+
+  // constructor(name: string, amount: string) {
+
+  // }same as what u have below
+  constructor(public name: string, public amount: number) {
+
+  }
+}
