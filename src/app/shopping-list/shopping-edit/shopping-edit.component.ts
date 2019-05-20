37a361e447edee127a71d8e373a463d0fe346d81
jumpingrock <kennethyeongky@gmail.com>
2019-05-20 18:54:42 +0800

    push obj into shoppinglist array

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
+
+
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,6 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  // @Output() ingredientAdded = new EventEmitter<{name: string, amount: number}>();
+
+
+  onAddItem() {
+    // to use the viewChild approch vs reading off input field
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredientAdded = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredientAdded);
+  }
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Ingredient } from '../shared/ingredient.mode';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -11,6 +11,9 @@ export class ShoppingListComponent implements OnInit {
     new Ingredient('Apple', 5),
     new Ingredient('Tomato', 10)
   ];
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
   constructor() { }
 
   ngOnInit() {
