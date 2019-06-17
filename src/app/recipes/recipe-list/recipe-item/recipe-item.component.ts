5fea4559be7e5fe4044682236fd1906ab5eee686
jumpingrock <kennethyeongky@gmail.com>
2019-06-17 22:06:50 +0800

    implemented service in recipe

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
@@ -7,15 +8,15 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
-  @Output() recipeSelected = new EventEmitter<string>();
 
 
-  constructor() { }
+
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
   }
-  onSelected(recipeSelect: string) {
-    this.recipeSelected.emit(recipeSelect);
+  onSelected() {
+    this.recipeService.recipeSelected.emit(this.recipe);
   }
 
 }
